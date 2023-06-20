363a90658be3db7a42a5bcc8365cd952addf5075
muqees99 <muqees465@gmail.com>
2023-06-21 02:54:21 +0500

    Print names using the List method

---
+++ q1.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<String> names = [
+    "Muqees",
+    'Ali',
+    'Ammar',
+    'Taha',
+    'Bilal',
+    'Osama',
+    'Abeer',
+    'Asad',
+    'Sara'
+  ];
+  print(names);
+}
