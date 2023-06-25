2edadd191cabe340870b1af523ac1ceb94e9db29
muqees99 <muqees465@gmail.com>
2023-06-25 19:58:35 +0500

    prints a new list with the elements in reverse order

---
+++ q12.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<String> names = [
+    'Muqees',
+    'Taha ',
+    'Bilal',
+    'Ammar',
+    'Saood',
+    'Ali',
+    'Abeer'
+  ];
+  List<String> reversedNames = List.of(names.reversed);
+  print('original List: $names');
+  print('Reverse Order: $reversedNames');
+}
