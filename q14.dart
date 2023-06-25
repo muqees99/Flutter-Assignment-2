810a4aafdebcd7f1ca28903b926c0328c681cfd7
muqees99 <muqees465@gmail.com>
2023-06-26 00:58:28 +0500

    prints a new list with the elements sorted in ascending order

---
+++ q14.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<int> orginalList = [2, 5, 4, 8, 1, 9, 12, 56, 34, 2, 4, 3];
+  print("Original List: $orginalList");
+  orginalList.sort();
+  List<int> sortedList = orginalList;
+  print('Sorted List: $sortedList');
+}
