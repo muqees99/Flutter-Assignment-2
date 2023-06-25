62a7bed495294dfd4e8eefbcffa0f9dba3489319
muqees99 <muqees465@gmail.com>
2023-06-26 03:25:23 +0500

    checking the elegibilty of student

--- q18.dart
+++ q18.dart
@@ -1,5 +1,5 @@
 void main() {
-  Map<String, dynamic> person = {"name": "John", "age": 2, "isStudent": true};
+  Map<String, dynamic> person = {"name": "John", "age": 24, "isStudent": true};
   if (person["age"] >= 18 && person["isStudent"] == true) {
     print("Eligible");
   } else {
